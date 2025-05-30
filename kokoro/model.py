141cb0fa71c874cf1fed2c12b89984663309a14f
nytopop <ericizoita@gmail.com>
2025-05-30 06:02:13 -0700

    add casts for fp16/bf16 support in forward()

--- kokoro/model.py
+++ kokoro/model.py
@@ -113,20 +113,20 @@ class KModel(torch.nn.Module):
         duration = torch.sigmoid(duration).sum(axis=-1) / speed
         pred_dur = torch.round(duration).clamp(min=1).long().squeeze()
         indices = torch.repeat_interleave(torch.arange(input_ids.shape[1], device=self.device), pred_dur)
-        pred_aln_trg = torch.zeros((input_ids.shape[1], indices.shape[0]), device=self.device)
+        pred_aln_trg = torch.zeros((input_ids.shape[1], indices.shape[0]), dtype=self.dtype, device=self.device)
         pred_aln_trg[indices, torch.arange(indices.shape[0])] = 1
         pred_aln_trg = pred_aln_trg.unsqueeze(0).to(self.device)
         en = d.transpose(-1, -2) @ pred_aln_trg
         F0_pred, N_pred = self.predictor.F0Ntrain(en, s)
-        t_en = self.text_encoder(input_ids, input_lengths, text_mask)
+        t_en = self.text_encoder(input_ids, input_lengths, text_mask).to(self.dtype)
         asr = t_en @ pred_aln_trg
         audio = self.decoder(asr, F0_pred, N_pred, ref_s[:, :128]).squeeze()
-        return audio, pred_dur
+        return audio.float(), pred_dur
 
     def forward(
         self,
         phonemes: str,
-        ref_s: torch.FloatTensor,
+        ref_s: torch.FloatTensor, # 1 x 256
         speed: float = 1,
         return_output: bool = False
     ) -> Union['KModel.Output', torch.FloatTensor]:
@@ -134,7 +134,7 @@ class KModel(torch.nn.Module):
         logger.debug(f"phonemes: {phonemes} -> input_ids: {input_ids}")
         assert len(input_ids)+2 <= self.context_length, (len(input_ids)+2, self.context_length)
         input_ids = torch.LongTensor([[0, *input_ids, 0]]).to(self.device)
-        ref_s = ref_s.to(self.device)
+        ref_s = ref_s.to(dtype=self.dtype, device=self.device)
         audio, pred_dur = self.forward_with_tokens(input_ids, ref_s, speed)
         audio = audio.squeeze().cpu()
         pred_dur = pred_dur.cpu() if pred_dur is not None else None
